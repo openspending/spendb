f97647175a13a77c420d99e82d76fe5ca0c2b8a8
Friedrich Lindenberg <friedrich@pudo.org>
2015-04-26 09:46:26 +0200

    Return a listing.

--- spendb/views/api_v3/source.py
+++ spendb/views/api_v3/source.py
@@ -1,18 +1,25 @@
 import logging
 
 from flask import Blueprint
+from archivekit import Source
 from apikit import jsonify, request_data
 
+from spendb.core import data_manager
 from spendb.lib.helpers import get_dataset
 from spendb.views.error import api_json_errors
 
 
 log = logging.getLogger(__name__)
-blueprint = Blueprint('datasets_api3', __name__)
+blueprint = Blueprint('sources_api3', __name__)
 
 
 @blueprint.route('/datasets/<dataset>/sources')
 @api_json_errors
 def index(dataset):
     dataset = get_dataset(dataset)
-    pass
+    package = data_manager.package(dataset.name)
+    sources = list(package.all(Source))
+    return jsonify({
+        'results': sources,
+        'total': len(sources)
+    })
