dbb2a2ffc2fe29c259b3aad712d1fffb5e109b39
Friedrich Lindenberg <friedrich@pudo.org>
2015-05-24 16:33:23 +0200

    Refactor model editor to use a columnn-based mapping.

--- spendb/static/js/admin.js
+++ spendb/static/js/admin.js
@@ -28,40 +28,71 @@ spendb.controller('AdminModelCtrl', ['$scope', '$http', '$window', '$timeout', '
   var modelApi = dataset.api_url + '/model';
   
   $scope.dataset = dataset;
-  $scope.structure = data.structure;
-  $scope.model = data.model;
+  $scope.samples = data.structure.samples;
 
-  // $scope.save = function(form) {
-  //   var dfd = $http.post(modelApi, $scope.model);
-  //   dfd.then(function(res) {
-  //     $scope.model = res.data;
-  //     flash.setMessage("Your changes have been saved!", "success");
-  //   }, validation.handle(form));
-  // };
-
-  var checkModel = function() {
-    $scope.model.measures = $scope.model.measures || {};
-    $scope.model.dimensions = $scope.model.dimensions || {};
-
-    var fields = [];
-    angular.forEach(data.structure.fields, function(v, k) {
-      fields.push(k);
-    });
-
-    var usedFields = [];
-    angular.forEach($scope.model.measures, function(v, k) {
-      usedFields.push(k.column);
-    });
-    angular.forEach($scope.model.dimensions, function(v, k) {
-      angular.forEach(k.attributes, function(v, k) {
-        usedFields.push(k.column);
-      });
-    });
-
-    //console.log(fields, usedFields);
+  $scope.save = function(form) {
+    var model = columnsToModel($scope.columns);
+    var dfd = $http.post(modelApi, model);
+    dfd.then(function(res) {
+      $scope.columns = modelToColumns(res.data);
+      flash.setMessage("Your changes have been saved!", "success");
+    }, validation.handle(form));
   };
 
-  checkModel();
+  var modelToColumns = function(model) {
+    var columns = [], usedFields = [];
+    model.measures = model.measures || {};
+    model.dimensions = model.dimensions || {};
+
+    console.log(model);
+
+    var pushColumn = function(data, concept) {
+      data.concept = concept;
+      usedFields.push(data.column);
+      columns.push(data);
+    };
+
+    for (var measure in model.measures) {
+      var m = model.measures[measure];
+      m.name = measure;
+      pushColumn(m, 'measure');
+    }
+
+    for (var dim in model.dimensions) {
+      var dimdata = model.dimensions[dim];
+      dimdata.name = dim;
+      for (var attr in dimdata.attributes) {
+        var a = dimdata.attributes[attr];
+        a.name = attr;
+        a.dimension = dimdata;
+        pushColumn(a, 'attribute');
+      }
+    }
+
+    for (var field in data.structure.fields) {
+      if (usedFields.indexOf(field) == -1) {
+        var fdata = data.structure.fields[field];
+        console.log(field);
+        var c = {
+          name: field, 
+          label: fdata.title,
+          column: field
+        };
+        pushColumn(c, 'attribute');
+      }
+    }
+
+    console.log(columns);
+    return columns;
+  };
+
+  var columnsToModel = function(columns) {
+    var model = {};
+    return model;
+  };
+
+  $scope.columns = modelToColumns(data.model);
+
   $scope.getCellClass = function(field, value) {
     var clazz = 'text';
     if (['integer', 'float', 'decimal'].indexOf(field.type) != -1) {
