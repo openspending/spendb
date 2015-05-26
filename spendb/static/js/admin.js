8c6a0762cfd412d4f75cfbb369daad581d342e44
Friedrich Lindenberg <friedrich@pudo.org>
2015-05-26 06:29:51 -0400

    fix up the autocomplete

--- spendb/static/js/admin.js
+++ spendb/static/js/admin.js
@@ -17,15 +17,28 @@ spendb.controller('AdminMetadataCtrl', ['$scope', '$q', '$http', '$location', '$
     var dfd = $q.defer(),
         params =  {q: query};
     $http.get('/accounts/_complete', {params: params}).then(function(es) {
-        dfd.resolve(es.data.results);
+      var accounts = []
+      for (var i in es.data.results) {
+        var account = es.data.results[i],
+            seen = false;
+        for (var j in $scope.managers.managers) {
+          var other = $scope.managers.managers[j];
+          if (other.name == account.name) {
+            seen = true;
+          }
+        }
+        if (!seen) {
+          accounts.push(account);
+        }
+      }
+      dfd.resolve(accounts);
     });
     return dfd.promise;
   };
 
   $scope.addAccount = function() {
     if ($scope.managers.fresh && $scope.managers.fresh.name) {
-      // Check if this account is already there.
-      $scope.managers.push($scope.managers);
+      $scope.managers.managers.push($scope.managers.fresh);
       $scope.managers.fresh = null;
     }
   };
