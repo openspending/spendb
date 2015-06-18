753ad8f81b0c35bf94f82db249dccc81142f1e7b
Friedrich Lindenberg <friedrich@pudo.org>
2015-06-18 15:13:23 +0200

    hack up the exporter a bit.

--- contrib/os_export/export.py
+++ contrib/os_export/export.py
@@ -3,9 +3,16 @@ import json
 import dataset
 from datetime import datetime
 
-DB_URI = 'postgresql://localhost/spendb'
+DB_URI = 'postgresql://localhost/openspending'
+OFFENERHAUSHALT_SETS = ['de-bund', 'de-bw', 'open_by', 'berlin_de',
+                        'de-bonn-planung', 'de-bonn', 'de-brandenburg-land',
+                        'de-bremen', 'open_gauting', 'de-nrw-cologne',
+                        'laatzen2014', 'stadt_lueneburg', 'open_bzmfr',
+                        'lhm_20150415', 'de-muenster-gesamt', 'open_bzobb',
+                        'hhos', 'open_rgb', 'open_bzswa', 'uelzen',
+                        'haushalt_ulm', 'haushaltwitten']
 
-engine = dataset.connect(DB_URI)
+engine = dataset.connect(os.environ.get('DATABASE_URI', DB_URI))
 
 
 def json_default(obj):
@@ -15,8 +22,23 @@ def json_default(obj):
 
 def get_mappings():
     for ds in list(engine['dataset']):
-        data = json.loads(ds['data'])
-        mapping = data.get('mapping')
+        if ds['name'] not in OFFENERHAUSHALT_SETS:
+            continue
+        ds['data'] = json.loads(ds['data'])
+
+        ds['languages'] = []
+        for lang in engine['dataset_language'].find(dataset_id=ds['id']):
+            ds['languages'].append(lang['code'])
+
+        ds['territories'] = []
+        for terr in engine['dataset_territory'].find(dataset_id=ds['id']):
+            ds['territories'].append(terr['code'])
+
+        ds['sources'] = []
+        for src in engine['source'].find(dataset_id=ds['id']):
+            ds['sources'].append(src)
+
+        mapping = ds['data'].get('mapping')
         if mapping is None or not len(mapping):
             continue
 
@@ -28,7 +50,7 @@ def get_queries():
         ds_name = ds['name']
         table_pattern = ds_name + '__'
         entry_table = '"' + table_pattern + 'entry"'
-        fields = [(entry_table + '.id', '_spendb_id')]
+        fields = [(entry_table + '.id', '_openspending_id')]
         joins = []
         for dim, desc in map.items():
             if desc.get('type') == 'compound':
@@ -47,7 +69,7 @@ def get_queries():
                 fields.append(('%s.name' % dim_table, dim))
             else:
                 fields.append(('%s.%s' % (entry_table, dim), dim))
-            
+
         select_clause = []
         for src, alias in fields:
             select_clause.append('%s AS "%s"' % (src, alias))
@@ -60,7 +82,8 @@ def get_queries():
             join_clause.append(qb)
         join_clause = ' '.join(join_clause)
 
-        yield ds, 'SELECT %s FROM %s %s' % (select_clause, entry_table, join_clause)
+        yield ds, 'SELECT %s FROM %s %s' % (select_clause, entry_table,
+                                            join_clause)
 
 
 def freeze_all():
@@ -68,13 +91,11 @@ def freeze_all():
     for ds, query in get_queries():
         try:
             path = os.path.join(out_base, ds['name'])
-            #print [query]
             if not os.path.isdir(path):
                 os.makedirs(path)
 
             ds_path = os.path.join(path, 'dataset.json')
             with open(ds_path, 'wb') as fh:
-                ds['data'] = json.loads(ds['data'])
                 json.dump(ds, fh, default=json_default, indent=2)
 
             res = engine.query(query)
